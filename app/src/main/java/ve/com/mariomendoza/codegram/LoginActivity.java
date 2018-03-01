cbf7204d03ee0f830806f5e92ab3200cdd883cfc
Mario Mendoza <mario.amg03@gmail.com>
2018-03-01 14:17:49 -0400

    Reto -1
    Crear un Intent del Login a el contenedor principal

--- app/src/main/java/ve/com/mariomendoza/codegram/LoginActivity.java
+++ app/src/main/java/ve/com/mariomendoza/codegram/LoginActivity.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import ve.com.mariomendoza.codegram.view.ContainerActivity;
 import ve.com.mariomendoza.codegram.view.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView reg_editext;
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,15 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent reg_intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(reg_intent);
+            }
+        });
+
+        login_button = (Button) findViewById(R.id.login_btn);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home_intent = new Intent(LoginActivity.this, ContainerActivity.class);
+                startActivity(home_intent);
             }
         });
 
