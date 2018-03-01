62d28f7339bc46508e63e582f42ca68aa2f2a71e
Mario Mendoza <mario.amg03@gmail.com>
2018-03-01 15:21:34 -0400

    Fragments en nuestro BotttomBar
    Veamos como integrar nuestros fragments

--- app/src/main/java/ve/com/mariomendoza/codegram/view/ContainerActivity.java
+++ app/src/main/java/ve/com/mariomendoza/codegram/view/ContainerActivity.java
@@ -1,15 +1,58 @@
 package ve.com.mariomendoza.codegram.view;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
 import ve.com.mariomendoza.codegram.R;
+import ve.com.mariomendoza.codegram.view.fragment.HomeFragment;
+import ve.com.mariomendoza.codegram.view.fragment.ProfileFragment;
+import ve.com.mariomendoza.codegram.view.fragment.SearchFragment;
+
 
 public class ContainerActivity extends AppCompatActivity {
+
+    private BottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        bottomBar = (BottomBar) findViewById(R.id.bottombar);
+        bottomBar.setDefaultTab(R.id.home);
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+                switch (tabId){
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .addToBackStack(null).commit();
+                        break;
+                    case R.id.search:
+                        SearchFragment searchFragment = new SearchFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,searchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.profile:
+                        ProfileFragment profileFragment = new ProfileFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+
+                }
+            }
+        });
+
+
+
     }
 }
