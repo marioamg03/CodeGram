608fa014fa6bbb23ce558e021ed16390dfccd13a
Mario Mendoza <mario.amg03@gmail.com>
2018-03-06 14:22:04 -0400

    Comportamientos dependientes en Material Design
    Un comportamientos dependientes, es un elemento es dependiente de otro en un evento.
    
    Para esto vamos a usar simple-view-behavior
    
    Agregamos la librería en gradle
    
    compile \'com.zoonref:simple-view-behavior:1.0\'
    
    
    Veamos cómo implementar esta librería.

--- app/src/main/java/ve/com/mariomendoza/codegram/view/fragment/ProfileFragment.java
+++ app/src/main/java/ve/com/mariomendoza/codegram/view/fragment/ProfileFragment.java
@@ -3,11 +3,19 @@ package ve.com.mariomendoza.codegram.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import ve.com.mariomendoza.codegram.R;
+import ve.com.mariomendoza.codegram.adapter.PictureAdapterRecyclerView;
+import ve.com.mariomendoza.codegram.model.Picture;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,38 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        showToolbar("",false,view);
+
+        RecyclerView pictureRecycler = (RecyclerView) view.findViewById(R.id.profileRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        pictureRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buildPictures(),R.layout.cardview_picture,getActivity());
+        pictureRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
     }
+
+
+    public ArrayList<Picture> buildPictures(){
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("https://www.marketingdirecto.com/wp-content/uploads/2015/08/londres-2.jpg","Mario Mendoza","3 días","12"));
+        pictures.add(new Picture("http://cadenaser00.epimg.net/ser/imagenes/2017/07/07/internacional/1499423602_279221_1499424341_noticia_normal.jpg","Carlos Sionchez","5 días","-4"));
+        pictures.add(new Picture("http://insights.la/wp-content/uploads/2015/08/g5-620x400.jpg","Liliana Colls","7 días","125"));
+        return  pictures;
+    }
+
+    public void showToolbar(String title, boolean upButton,View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.profileToolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
 
 }
