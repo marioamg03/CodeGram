1bb239038238b54086ec62fc33e841fc955bf673
Mario Mendoza <mario.amg03@gmail.com>
2018-03-06 16:45:20 -0400

    Desafío 02
    Ya conoces intents, así programa cuando un usuario de tap en el logotipo debes dirigirlo a una pagina web. SOLUCIONADO!

--- app/src/main/java/ve/com/mariomendoza/codegram/LoginActivity.java
+++ app/src/main/java/ve/com/mariomendoza/codegram/LoginActivity.java
@@ -1,11 +1,13 @@
 package ve.com.mariomendoza.codegram;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -17,6 +19,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextView reg_editext;
     private Button login_button;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,18 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(home_intent);
             }
         });
+
+        logo = (ImageView) findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "http://www.mariomendoza.com.ve";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
 
     }
 }
