8fca178d5d59e6ccd5a733acf312e5733d52ed68
Mario Mendoza <mario.amg03@gmail.com>
2018-03-06 09:20:50 -0400

    Collapsing Toolbar Layout, AppBarLayout
    CoordinatorLayout, ayuda a especificar comportamientos de interacciones en los views hijos, ayuda a controlar vistas flotantes.
    
    CollapsingToolbarLayout, es un wrapper para Toolbar el cual implementa el efecto collapsable en toolbar y los elementos hijos.
    
    Veamos cómo implementar esto en PlatziGram.

---
+++ app/src/main/java/ve/com/mariomendoza/codegram/view/PictureDetailActivity.java
@@ -0,0 +1,15 @@
+package ve.com.mariomendoza.codegram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ve.com.mariomendoza.codegram.R;
+
+public class PictureDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+    }
+}
