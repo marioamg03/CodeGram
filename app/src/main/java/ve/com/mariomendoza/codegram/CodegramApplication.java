83c2f508d6f03083fa03f61c4e72b8bf7f6a5e83
Mario Mendoza <mario.amg03@gmail.com>
2018-04-16 18:20:30 -0400

    Firebase Authentication Correo y Contraseña en Platzigram

---
+++ app/src/main/java/ve/com/mariomendoza/codegram/CodegramApplication.java
@@ -0,0 +1,15 @@
+package ve.com.mariomendoza.codegram;
+
+import android.app.Application;
+
+public class CodegramApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+
+}
--- app/src/main/java/ve/com/mariomendoza/codegram/view/RegisterActivity.java
+++ app/src/main/java/ve/com/mariomendoza/codegram/view/RegisterActivity.java
@@ -1,18 +1,75 @@
 package ve.com.mariomendoza.codegram.view;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import ve.com.mariomendoza.codegram.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "CreateAccountActivity";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private Button joinUsButton;
+    private TextInputEditText emailEdt, passwordEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         showToolbar(getResources().getString(R.string.reg_toolbar),true);
+
+        emailEdt     = (TextInputEditText) findViewById(R.id.reg_correo);
+        passwordEdt  = (TextInputEditText) findViewById(R.id.reg_password);
+        joinUsButton = (Button) findViewById(R.id.reg_btn);
+
+
+        joinUsButton.setOnClickListener(new View.OnClickListener() {
+            String email    = (emailEdt).getText().toString();
+            String password = (passwordEdt).getText().toString();
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(getParent(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this,"Cuenta creada",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(RegisterActivity.this,"Ocurrio un error",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null){
+                    Log.w(TAG,"Usuario Logueado"+firebaseUser.getEmail());
+                }else{
+                    Log.w(TAG,"Usuario NO Logueado");
+                }
+            }
+        };
     }
 
     public void showToolbar(String title, boolean upButton){
@@ -20,5 +77,17 @@ public class RegisterActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
     }
 }
