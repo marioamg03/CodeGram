65e084bc6b78558156f599d8bc05aa7f22b44a91
Mario Mendoza <mario.amg03@gmail.com>
2018-05-09 13:05:02 -0400

    Implementando Facebook Login

--- app/src/main/java/ve/com/mariomendoza/codegram/CodegramApplication.java
+++ app/src/main/java/ve/com/mariomendoza/codegram/CodegramApplication.java
@@ -2,12 +2,17 @@ package ve.com.mariomendoza.codegram;
 
 import android.app.Application;
 
+import com.facebook.FacebookSdk;
+
 public class CodegramApplication extends Application {
 
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
     }
 
 
--- app/src/main/java/ve/com/mariomendoza/codegram/login/view/LoginActivity.java
+++ app/src/main/java/ve/com/mariomendoza/codegram/login/view/LoginActivity.java
@@ -2,15 +2,35 @@ package ve.com.mariomendoza.codegram.login.view;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.Login;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import ve.com.mariomendoza.codegram.R;
 import ve.com.mariomendoza.codegram.login.presenter.LoginPresenter;
@@ -26,16 +46,22 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
 
     private TextView reg_editext;
     private Button login_button;
+    private LoginButton loginFacebook;
     private ImageView logo;
 
     private LoginPresenter loginPresenter;
 
+    private CallbackManager callbackManager;
 
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
 
         loginPresenter = new LoginPresenterImpl(this);
 
@@ -61,6 +87,29 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
             }
         });
 
+        loginFacebook.setReadPermissions(Arrays.asList("email"));
+        loginFacebook = (LoginButton) findViewById(R.id.login_facebook);
+        loginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.w("FACEBOOK","Logueo de Facebook Exitoso " + loginResult.getAccessToken().getApplicationId());
+                signInFacebookFirebase(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.w("FACEBOOK","Logueo de Facebook Cancelado");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.w("FACEBOOK","OCURRIO UN ERROR CODIGO: " + error.toString());
+                error.printStackTrace();
+            }
+        });
+
+
+
         logo = (ImageView) findViewById(R.id.logo);
         logo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +122,21 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
         });
 
 
+    }
+
+    private void signInFacebookFirebase(AccessToken accessToken) {
+        AuthCredential authCredential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    goHome();
+                    Toast.makeText(LoginActivity.this, "Login Facebook Exitoso!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login Facebook Erroneo!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
